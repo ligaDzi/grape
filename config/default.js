6109f9349aa0601fe306e99e1df1f9bfdc201cc6
Ligaga <asergy34@gmail.com>
2018-06-10 12:05:14 +0300

    Instal Cloudinary

--- config/default.js
+++ config/default.js
@@ -2,7 +2,7 @@ module.exports = {
   port: process.env.PORT || 3000,
   secret: 'mysecret',
   mongoose: {
-    uri:    process.env.MONGO_URL ||  'mongodb://localhost/grape',
+    uri: process.env.MONGO_URL ||  'mongodb://localhost/grape',
     options: {
       server: {
         socketOptions: {
